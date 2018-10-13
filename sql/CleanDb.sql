0d7275f45ac346f60448e989afc7c908a1798de9
mycodeclub <ankitsahay1990@gmail.com>
2018-10-13 09:05:37 +0530

    Clean database

---
+++ sql/CleanDb.sql
@@ -0,0 +1,12 @@
+DELETE FROM BankMaster
+DELETE FROM CadreMaster
+DELETE FROM PFAMaster
+DELETE FROM ProgrammeMaster
+DELETE FROM RankMaster
+DELETE FROM SectionMaster
+DELETE FROM StationMaster
+DELETE FROM UnitResearchMaster
+DELETE FROM UnitServicesMaster
+DELETE FROM EmployeeGI
+DELETE FROM CustomerMaster
+DELETE FROM UserMaster
